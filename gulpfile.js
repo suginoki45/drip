4abce25b78ce4049c48604f4af85398fec6621d8
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2018-10-15 16:39:55 +0900

    Make it possible to separate tasks of gulp in development environment and production environment.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,17 @@ const gulp = require('gulp');
 const runSequence = require('run-sequence');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const gulpif = require('gulp-if');
+const minimist = require('minimist');
+const envSettings = {
+  string: 'env',
+  default: {
+    env: process.env.NODE_ENV || 'development',
+  },
+};
+const options = minimist(process.argv.slice(2), envSettings);
+const production = options.env === 'production';
 
 // image
 const imagemin = require('gulp-imagemin');
@@ -15,7 +26,6 @@ const imageminGif = require('imagemin-gifsicle');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
 
 // js
 const babel = require('gulp-babel');
@@ -40,6 +50,7 @@ const dir = {
     svg: 'src/svg',
   },
   dist: {
+    root: 'dist',
     css: 'dist/css',
     js: 'dist/js',
     img: 'dist/img',
@@ -47,23 +58,31 @@ const dir = {
   },
 };
 
+const config = {
+  sassOptions: {
+    includePaths: ['node_modules/'],
+    outputStyle: 'compressed',
+  },
+  envProduction: production,
+};
+
 // Clean directory
 gulp.task('clean', callback => {
-  del(['img'], callback);
+  return del([`${dir.dist.root}`], callback);
 });
 
 /**
  * Build defaultTasks
  */
 gulp.task('build', ['clean'], callback => {
-  return runSequence(['scss', 'img'], callback);
+  return runSequence(['scss', 'babel', 'img'], callback);
 });
 
 /**
  * Minify images
  */
 gulp.task('img', () => {
-  gulp
+  return gulp
     .src(`${dir.src.img}/**/*.+(jpg|jpeg|png|gif)`)
     .pipe(
       imagemin([
@@ -83,7 +102,7 @@ gulp.task('img', () => {
  * Build SVG
  */
 gulp.task('svg', function() {
-  gulp
+  return gulp
     .src(`${dir.src.svg}/icon/*.svg`)
     .pipe(svgmin())
     .pipe(svgstore({ inlineSvg: true }))
@@ -99,7 +118,7 @@ gulp.task('svg', function() {
     .pipe(gulp.dest(dir.dist.svg));
 });
 gulp.task('svg2png', function() {
-  gulp
+  return gulp
     .src(`${dir.src.svg}/icon/*.svg`)
     .pipe(svg2png(3))
     .pipe(rename({ prefix: 'icon.svg.' }))
@@ -116,8 +135,9 @@ gulp.task('scss', () => {
     .pipe(
       plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
     )
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulpif(!config.envProduction, sourcemaps.init()))
+    .pipe(gulpif(config.envProduction, sass({ outputStyle: 'compressed' })))
+    .pipe(gulpif(!config.envProduction, sass({ outputStyle: 'expanded' })))
     .pipe(
       autoprefixer({
         browsers: ['last 2 version'],
@@ -125,7 +145,7 @@ gulp.task('scss', () => {
         cascade: false,
       })
     )
-    .pipe(sourcemaps.write('./maps/'))
+    .pipe(gulpif(!config.envProduction, sourcemaps.write('./maps/')))
     .pipe(gulp.dest(dir.dist.css));
 });
 
